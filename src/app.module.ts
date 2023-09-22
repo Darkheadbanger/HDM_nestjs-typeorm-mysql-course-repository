96a40e7048f4e5b50e0256ee0b5e1218efea8a73
David Bouhaben <davidbouhaben@yahoo.co.id>
2023-09-22 16:26:33 +0200

    Connected to database and entities created

---
+++ src/app.module.ts
@@ -0,0 +1,39 @@
+import { Module, OnModuleInit } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './typeorm/entities/User';
+import { DataSource } from 'typeorm';
+import { UsersModule } from './users/users.module';
+// Give me the code to use DataSource
+
+@Module({
+  imports: [
+    // TypeOrmModule.forRoot() method is used to connect to the database.
+    // In this case to MySQL database.
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'testuser',
+      password: 'testuser',
+      database: 'nestjs_mysql_tutorial',
+      entities: [User], // Il faut connecter la propriété entities à l'entité User du doissier src/typeorm/entities/User.ts pour que l'entité soit reconnue par le module TypeOrmModule.
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements OnModuleInit {
+  constructor(private readonly dataSource: DataSource) {}
+  async onModuleInit() {
+    try {
+      await this.dataSource.query('SELECT 1');
+      console.log('Database connection is successful.');
+    } catch (error) {
+      console.error('Database connection is failed.');
+    }
+  }
+}
---
+++ src/typeorm/entities/User.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+// @Entity is a decorator that marks a class as a type of entity.
+// The meaning of Entity is a table in the database.
+@Entity({ name: 'users' })
+export class User {
+  // PrimaryGeneratedColumn is a decorator that marks a column as a primary key.
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column() // La date de création est automatiquement ajoutée
+  createdAt: string;
+
+  @Column({ nullable: true }) // La raison que la propriété est nullable est que la propriété est facultative. Une stratégie pour que l'authentification puisse être facultative ou obligatoire.
+  authStrategy: string;
+}
